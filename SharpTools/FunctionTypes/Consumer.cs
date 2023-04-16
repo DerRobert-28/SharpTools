c6d6aab45ea41be3b28316ed75283d0bf7b25e87
Der Robert <erictainment@live.de>
2023-04-16 02:45:37 +0200

    remove consumer/supplier and provide several consumer and supplier classes

--- SharpTools/FunctionTypes/Consumer.cs
+++ SharpTools/FunctionTypes/Consumer.cs
@@ -1,47 +1,5 @@
 ﻿namespace DerRobert28.SharpTools.FunctionTypes {
 
-	/*
-	using System;
-
-	public class Consumer {
-
-		public static Consumer<bool> ofBool(Consumer<bool>.Delegate consumer)
-			=> Consumer<bool>.of(consumer);
-
-		public static Consumer<byte> ofByte(Consumer<byte>.Delegate consumer)
-			=> Consumer<byte>.of(consumer);
-
-		public static Consumer<short> ofShort(Consumer<short>.Delegate consumer)
-			=> Consumer<short>.of(consumer);
-
-		public static Consumer<int> ofInt(Consumer<int>.Delegate consumer)
-			=> Consumer<int>.of(consumer);
-
-		public static Consumer<long> ofLong(Consumer<long>.Delegate consumer)
-			=> Consumer<long>.of(consumer);
-
-		public static Consumer<float> ofFloat(Consumer<float>.Delegate consumer)
-			=> Consumer<float>.of(consumer);
-
-		public static Consumer<double> ofDouble(Consumer<double>.Delegate consumer)
-			=> Consumer<double>.of(consumer);
-
-		public static Consumer<decimal> ofDouble(Consumer<decimal>.Delegate consumer)
-			=> Consumer<decimal>.of(consumer);
-
-		public static Consumer<char> ofChar(Consumer<char>.Delegate consumer)
-			=> Consumer<char>.of(consumer);
-
-		public static Consumer<string> ofString(Consumer<string>.Delegate consumer)
-			=> Consumer<string>.of(consumer);
-
-		public static Consumer<Exception> ofException(Consumer<Exception>.Delegate consumer)
-			=> Consumer<Exception>.of(consumer);
-
-	}
-	*/
-
-
 	public class Consumer<T> {
 
 		public delegate void Delegate(T value);
